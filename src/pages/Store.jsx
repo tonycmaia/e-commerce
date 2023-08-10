7d880facb0f2d08672ab05452862f2e3f6dd3395
tonycmaia <tonycmaia@gmail.com>
2023-08-10 18:49:40 -0300

    second commit

--- src/pages/Store.jsx
+++ src/pages/Store.jsx
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from "react";
+import { BsFillCartCheckFill, BsFillCartPlusFill } from 'react-icons/bs'
 
 export const Store = () => {
     const [data, setData] = useState([])
+    const [cart, setCart] = useState([])
     console.log(data)
     useEffect(() => {
         const fetchApi = async () => {
@@ -13,6 +15,17 @@ export const Store = () => {
         fetchApi();
     }, [])
 
+    const handleClick = (obj) => {
+        const element = cart.find((e) => e.id === obj.id)
+        if (element) {
+            const arrFilter = cart.filter((e) => e.id !== obj.id)
+            setCart(arrFilter)
+        } else {
+            setCart([...cart, obj])
+        }
+
+    }
+
     return (
         <div>
             <h1>STORE</h1>
@@ -21,9 +34,17 @@ export const Store = () => {
                     data.map((e) => (
                         <div key={e.id}>
                             <h4>{e.title}</h4>
-
                             <img src={e.thumbnail} alt="" />
                             <h4>{e.price}</h4>
+                            <button onClick={handleClick(e)} >
+                                {
+                                    cart.some((itemCart) => itemCart.id === e.id) ? (
+                                        <BsFillCartCheckFill />)
+                                        : (
+                                            <BsFillCartPlusFill />
+                                        )
+                                }
+                            </button>
                         </div>
 
                     ))
